514af69b1b82e60aecd3eacfba848ad9d1c7927a
XBTFinex <xbtfinex@gmail.com>
2019-08-26 08:30:39 -0400

    add menu, add images, add contact

--- js/app.js
+++ js/app.js
@@ -9,6 +9,12 @@ var SERVICE_DB={
           "O controlo de baratas é um desafio constante! As baratas são uma ameaça singular para as instalações onde se processa alimentos, dada a sua resistência e a sua grande capacidade de reprodução.",
           "A EDD Serviços dispõe de uma ampla gama de produtos excelentes e soluções completas para o controlo de baratas, com satisfação garantida.",
           "Peça uma avaliação sem compromisso. Os nossos técnicos estão prontos para oferecer-lhe apoio técnico com profissionalismo sempre que precisar.",          
+      ],
+      eList: [
+          './image/IMG_9830.JPG',
+          './image/IMG_9834.JPG',
+          './image/IMG_7245.JPG',
+          './image/IMG_7246.JPG',
       ]
       
     },
@@ -24,11 +30,15 @@ var SERVICE_DB={
     PROD_C03: {
         id: "PROD_C03",
       "title": "Controlo de Roedores",
-      "img": "./image/IMG_7219.JPG",
+      "img": "./image/rato.png",
       description: [
           "Para um controlo eficaz de roedores é fundamental a correcta identificação da espécie, o conhecimento da sua biologia, de seus hábitos comportamentais, suas habilidades e capacidades físicas. É preciso uma correcta avaliação do espaço a tratar.",
           "Técnicas ou métodos de controlo de roedores ineficazes podem ter um efeito inverso, resultando no aumento das colónias, agravando ainda mais a situação.",
           "Conheça os nossos planos de controlo de roedores. Peça uma avaliação sem compromisso. Os nossos técnicos estão prontos para oferecer-lhe apoio técnico com profissionalismo sempre que precisar.",          
+      ],
+      eList: [
+        './image/Rato-equip.jpg',
+        './image/roedores.JPG'
       ]
     },
     PROD_C04: {
@@ -39,20 +49,25 @@ var SERVICE_DB={
           "As Traças não representam propriamente um risco para a saúde. São uma Praga maléfica nas Casas pelos danos sérios que as suas larvas causam em vestuário, tecidos, peles, couros, tapetes e carpetes.",
           "Uma pequena infestação de Traças pode ser tratada com produtos comercias para amadores, mas uma infestação muito implantada requer geralmente assistência profissional.",
           "Conheça as nossas soluções de controlo das traças. Todos os nossos tratamentos são seguros para a sua família e para os seus animais de estimação e respeitam o meio ambiente. Peça uma avaliação sem qualquer compromisso.",
-          "Os mosquitos representam uma grande ameaça à saúde pública, pois podem transmitir várias doenças como a febre amarela, dengue, malária, zika e alguns tipos de encefalite, filariose, etc.",
-          "O controlo eficaz de mosquitos apresenta uma série de desafios tendo em conta as suas características físicas , biológicas e comportamentais. O controlo de mosquitos implica uma rigorosa análise técnica das áreas a serem tratadas, a escolha do método e técnicas de controlo adequadas. ",
-          "No caso de tratamento químico, a escolha do tipo de formulação a aplicar,  sua concentração e o tipo de equipamento a utilizar devem obedecer às regras mais rigorosas, na completa observância pela preservação do meio ambiente e a salvaguarda de espécies não alvo.",
-          "A EDD Serviços dispõe de uma ampla gama de produtos excelentes e soluções completas para o controlo de mosquitos nas suas instalações, com satisfação garantida.",
-          "Peça uma avaliação sem compromisso. Os nossos técnicos estão prontos para oferecer-lhe apoio técnico com profissionalismo sempre que precisar."
       ]
     },
     PROD_C05: {
         id: "PROD_C05",
-      "title": "Controlo de Mosquitos",
-      "img": "./image/svs/IMG_7286.JPG",
-      description: [
-          ""
-      ]
+        "title": "Controlo de Mosquitos",
+        "img": "./image/12640.png",
+        description: [
+            "Os mosquitos representam uma grande ameaça à saúde pública, pois podem transmitir várias doenças como a febre amarela, dengue, malária, zika e alguns tipos de encefalite, filariose, etc.",
+            "O controlo eficaz de mosquitos apresenta uma série de desafios tendo em conta as suas características físicas , biológicas e comportamentais. O controlo de mosquitos implica uma rigorosa análise técnica das áreas a serem tratadas, a escolha do método e técnicas de controlo adequadas. ",
+            "No caso de tratamento químico, a escolha do tipo de formulação a aplicar,  sua concentração e o tipo de equipamento a utilizar devem obedecer às regras mais rigorosas, na completa observância pela preservação do meio ambiente e a salvaguarda de espécies não alvo.",
+            "A EDD Serviços dispõe de uma ampla gama de produtos excelentes e soluções completas para o controlo de mosquitos nas suas instalações, com satisfação garantida.",
+            "Peça uma avaliação sem compromisso. Os nossos técnicos estão prontos para oferecer-lhe apoio técnico com profissionalismo sempre que precisar."
+        ],
+        eList: [
+            './image/YGT1.jpg',
+            './image/YGT2.jpg',
+            './image/svs/IMG_7286.JPG',
+            './image/IMG_7280.JPG'
+        ]
 
     },
     PROD_C06: {
@@ -64,6 +79,9 @@ var SERVICE_DB={
           "No caso de tratamento químico, a escolha do tipo de formulação a aplicar, a sua concentração e o tipo de equipamento a utilizar devem obedecer às regras mais rigorosas, na completa observância pela preservação do meio ambiente e a salvaguarda de espécies não alvo.",
           "A EDD Serviços dispõe de uma ampla gama de produtos excelentes e soluções completas para o controlo de mosquitos nas suas instalações, com satisfação garantida.",
           "Peça uma avaliação sem compromisso. Os nossos técnicos estão prontos para oferecer-lhe apoio técnico com profissionalismo sempre que precisar."
+      ],
+      eList: [
+          './image/IMG_7252.JPG'
       ]
     },
     PROD_C07: {
@@ -96,6 +114,12 @@ var SERVICE_DB={
           "As aves podem ser consideradas verdadeiras pragas e causadoras incómodo e de inúmeros danos materiais para os edifícios onde pousam e  nidificam. Podem mesmo ser transmissoras de doenças como a  salmonelose, toxoplasmose e pneumonia.",
           "A EDD – Serviços disponibiliza serviços de dissuasão de pássaros, respeitando as normas ambientais e de protecção de animais. Os nossos produtos para controlo de pássaros são 100% naturais, não tóxicos, seguros para o homem, não causando quaisquer danos para espécies não alvo ou o  meio ambiente.",
           "Contacte-nos agora  e  tenha um suporte  técnico gratuito ou  peça uma avaliação das suas instalações sem qualquer compromisso."
+      ],
+      eList: [
+          './image/IMG_5503.JPG',
+          './image/IMG_5509.JPG',
+          './image/IMG_5511.JPG',
+          './image/bye-birds.jpg',
       ]
     },
     PROD_S10: {
@@ -111,7 +135,8 @@ var SERVICE_DB={
 };
 
 var conf = {
-    PUBLIC_API: "https://publica.enterstarts.com"
+    PUBLIC_API: "http://127.0.0.1:9000"
+    // PUBLIC_API: "https://publica.enterstarts.com"
 };
 
 angular.module('app')
@@ -155,6 +180,50 @@ angular.module('app')
         });
     }
 })
+.controller("ContactCtrl", function ($http, $scope) {
+    var urlParams = new URLSearchParams(window.location.search);
+    var prodCode=urlParams.get("code");
+
+    $scope.obj={
+        msg: 'Ola, gostaria de saber mais informações'
+    };
+    $scope.is_done=false;
+
+    $scope.submit = function (form) {
+        if (form.$invalid) {
+            console.info("bad form", form);
+            return;
+        }
+
+        $scope.loading_request=true;
+        $scope.success=false;
+        $scope.error=false;
+
+        var obj={
+            org_id: '1a01bde072f04dac',
+            user_name: $scope.obj.name,
+            user_email: $scope.obj.email,
+            user_phone: $scope.obj.phone,
+            msg: $scope.obj.msg,
+            channel: 'WEBSITE'
+        };
+
+        $http.post(conf.PUBLIC_API+"/send-contact-msg", obj)
+        .then(function (Resp) {
+            console.info("response: ", Resp);
+            var obj=Resp.data;
+            $scope.loading_request=false;
+            $scope.success=true;
+            $scope.is_done=true;
+
+        }, function (Err) {
+            console.error("Err: ", Err);     
+            $scope.loading_request=false;
+            $scope.error=true;       
+        });
+    }
+})
+
 .controller("ServiceItemCtrl", function ($scope) {
     var urlParams = new URLSearchParams(window.location.search);
     var prodCode=urlParams.get("code");
